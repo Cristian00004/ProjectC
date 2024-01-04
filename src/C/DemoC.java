a2aa154c79820f9f9f08d48610fa97a32fae7ccf
Cree <Cree@cristians-air.attlocal.net>
2024-01-04 14:46:04 -0800

    Monkey fell

--- src/C/DemoC.java
+++ src/C/DemoC.java
@@ -6,5 +6,7 @@ public class DemoC {
 		System.out.println("Wednesday");
 		
 		System.out.println("There are monkeys in the tree");
+		
+		System.out.println("One of the monkeys fell from the tree");
 	}
 }
